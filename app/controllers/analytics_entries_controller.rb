b55c9d5a1ea3887796224e6644dc4f06478e3cb6
Matt Vanderpol <matt@vanderpol.net>
2014-10-12 16:50:09 -0700

    Get sharding symbol based on account

--- app/controllers/analytics_entries_controller.rb
+++ app/controllers/analytics_entries_controller.rb
@@ -1,9 +1,8 @@
 class AnalyticsEntriesController < ApplicationController
-
   def track_entry
     account = Account.find(params[:account_id])
     site = Site.find(params[:site_id])
-    Octopus.using(:shard_2) do
+    Octopus.using(ShardingHelper.shard_symbol_for_key account.sharding_key) do
       @entry = AnalyticsEntry.new( { user_agent: request.user_agent, url: request.referer, site_id: site.id } )
       @entry.save
     end
