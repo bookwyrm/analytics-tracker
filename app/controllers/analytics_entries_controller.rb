ae18b178a05a3106ea5733b4a25fd399eb35b233
Matt Vanderpol <matt@vanderpol.net>
2014-10-12 16:45:47 -0700

    Need object instantiation in the using block

--- app/controllers/analytics_entries_controller.rb
+++ app/controllers/analytics_entries_controller.rb
@@ -3,8 +3,8 @@ class AnalyticsEntriesController < ApplicationController
   def track_entry
     account = Account.find(params[:account_id])
     site = Site.find(params[:site_id])
-    @entry = AnalyticsEntry.new( { user_agent: request.user_agent, url: request.referer, site_id: site.id } )
     Octopus.using(:shard_2) do
+      @entry = AnalyticsEntry.new( { user_agent: request.user_agent, url: request.referer, site_id: site.id } )
       @entry.save
     end
     render plain: @entry.inspect
