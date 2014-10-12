f4d3fe320a7c0808faf41e2b509409d4d68fedbb
Matt Vanderpol <matt@vanderpol.net>
2014-10-12 10:58:56 -0700

    Ensure that proper params are passed to the registration_form
    Allow the company_name field to be passed through to the registration_form.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
+    configure_permitted_parameters
     super
   end
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :company_name
+  end
 
   def build_resource(hash = nil)
     self.resource = registration_form = RegistrationForm.new(user: User.new, account: Account.new)
