7343a2d5b75bc1456a63ded7a3a6de365d15c04b
Matt Vanderpol <matt@vanderpol.net>
2014-10-12 16:39:53 -0700

    Helper function to determine shard to write to

--- lib/sharding_helper.rb
+++ lib/sharding_helper.rb
@@ -4,4 +4,8 @@ module ShardingHelper
   def ShardingHelper.generate_sharding_key(string)
     MurmurHash3::V32.murmur3_32_str_hash(string)
   end
+
+  def ShardingHelper.shard_symbol_for_key(sharding_key, symbol_prefix = 'shard_')
+    "#{symbol_prefix}#{sharding_key % 4}".to_sym
+  end
 end
