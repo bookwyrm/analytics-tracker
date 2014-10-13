5d76e9cee6aaec4ab003e3bfe624c4f667cf49d3
Matt Vanderpol <matt@vanderpol.net>
2014-10-12 21:59:04 -0700

    Increment the entry_count when we add a new entry to support cache expiration.

--- app/controllers/analytics_entries_controller.rb
+++ app/controllers/analytics_entries_controller.rb
@@ -2,6 +2,11 @@ class AnalyticsEntriesController < ApplicationController
   def track_entry
     account = Account.find(params[:account_id])
     site = Site.find(params[:site_id])
+    # FIXME This reduces some benefit from sharding - need to sort out
+    # how to balance with caching benefit. Probably site should be
+    # sharded too
+    site.entry_count += 1
+    site.save
     Octopus.using(ShardingHelper.shard_symbol_for_key account.sharding_key) do
       @entry = AnalyticsEntry.new( { user_agent: request.user_agent, url: request.referer, site_id: site.id } )
       @entry.save
